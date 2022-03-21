f8455a99659d28fb07593f90ced17c278e9cfa2a
Emre Daştan <74495003+EmreDastan@users.noreply.github.com>
2022-03-22 01:37:53 +0300

    Add files via upload

---
+++ Safe/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Safe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(3, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(74, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(145, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(3, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 57);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(74, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 57);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(145, 67);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 57);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 57);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Location = new System.Drawing.Point(74, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 57);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(145, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(65, 57);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panel1.Controls.Add(this.button_0);
+            this.panel1.Controls.Add(this.button_reset);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(372, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(215, 257);
+            this.panel1.TabIndex = 11;
+            // 
+            // button_0
+            // 
+            this.button_0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button_0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_0.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_0.Location = new System.Drawing.Point(107, 193);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(103, 57);
+            this.button_0.TabIndex = 12;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = false;
+            this.button_0.Click += new System.EventHandler(this.button_0_Click);
+            // 
+            // button_reset
+            // 
+            this.button_reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.button_reset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_reset.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_reset.Location = new System.Drawing.Point(3, 193);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(98, 57);
+            this.button_reset.TabIndex = 11;
+            this.button_reset.Text = "AC";
+            this.button_reset.UseVisualStyleBackColor = false;
+            this.button_reset.Click += new System.EventHandler(this.button_reset_Click);
+            // 
+            // Display
+            // 
+            this.Display.BackColor = System.Drawing.Color.Black;
+            this.Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Display.ForeColor = System.Drawing.Color.Aqua;
+            this.Display.Location = new System.Drawing.Point(54, 58);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(258, 112);
+            this.Display.TabIndex = 12;
+            this.Display.Text = "LOCKED";
+            this.Display.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(79, 204);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(696, 426);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "TheGalactical-Safe";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Display;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Button button_0;
+    }
+}
+
---
+++ Safe/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Safe
+{
+    public partial class Form1 : Form
+    {
+        /* Uncomment button_newpass_clicked method, add it to GUI
+         * and fix the problem.
+        Default password is "pass" variable and equals to 2854.
+        */
+        bool newpassclicked = false;
+        bool unlocked = false;
+        private int passinput;
+        private int setcounter = 0;
+        private string password = "";
+        private int digitlimit = 4;
+        private int count = 0;
+        private int pass = 2854;
+        private int trylimit = 6;
+        private int trying = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "1";
+                count++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "2";
+                count++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "3";
+                count++;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "4";
+                count++;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "5";
+                count++;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "6";
+                count++;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "7";
+                count++;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "8";
+                count++;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "9";
+                count++;
+            }
+        }
+
+        /*private void button_newpass_Click(object sender, EventArgs e)
+        {
+            newpassclicked = true;
+            count = 0;
+            password = "";
+            Display.Text = "Current Pass :";
+
+        }*/
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            password = "";
+            Display.Text = "LOCKED";
+            setcounter = 0;
+            unlocked = false;
+        }
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            if (count <= digitlimit)
+            {
+                password += "0";
+                count++;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!unlocked)
+                {
+                    passinput = Convert.ToInt32(password);
+                    if (!newpassclicked && setcounter == 0)
+                    {
+                        if ((passinput == pass) && (count == digitlimit) && (trying < trylimit))
+                        {
+                            // True pass
+                            Display.Text = "UNLOCKED";
+                            unlocked = true;
+                        }
+                        if ((passinput != pass) && (count == digitlimit) && (trying < trylimit))
+                        {
+                            // Wrong pass
+                            Display.Text = "WRONG PASS";
+                            trying++;
+                        }
+                        if ((count != digitlimit) && (trying < trylimit))
+                        {
+                            // You can only enter 4 digits length password
+                            Display.Text = "ONLY 4 DIGITS";
+                            count = 0;
+                        }
+                        if (trying >= trylimit)
+                        {
+                            // Max amount of tries reached
+                            Display.Text = "OUT OF TRIES";
+                        }
+                    }
+                    else
+                    {
+                        setcounter++;
+                        passinput = Convert.ToInt32(password);
+                        if ((passinput == pass) && (trying < trylimit))
+                        {
+                            // True pass
+                            if ((count == digitlimit) && (setcounter > 1))
+                            {
+                                pass = passinput;
+                                setcounter = 0;
+                                Display.Text = "LOCKED";
+                                count = 0;
+                                password = "";
+                                trying = 0;
+                            }
+                            else if (setcounter == 1)
+                            {
+                                Display.Text = "NEW PASSWORD:";
+                                count = 0;
+                                password = "";
+                            }
+
+
+                        }
+                        if ((count != digitlimit) && (setcounter == 1))
+                        {
+                            Display.Text = "MUST BE 4 DIGITS";
+                            count = 0;
+                        }
+                        if ((passinput != pass) && (trying < trylimit) && (setcounter == 1))
+                        {
+                            // Wrong pass
+                            Display.Text = "WRONG PASS";
+                            trying++;
+                        }
+                        if (trying >= trylimit && (setcounter == 1))
+                        {
+                            // Max amount of tries reached
+                            Display.Text = "OUT OF TRIES";
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                Display.Text = "CANNOT DO THAT";
+            }
+
+
+        }
+    }
+}
